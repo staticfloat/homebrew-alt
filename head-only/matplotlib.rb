8ceb29c15e8efae244eb7770c6ad9f5ab75e400a
Elliot Saba <staticfloat@gmail.com>
2012-04-07 16:23:21 -0700

    Formula for matplotlib, due to incompatibility of matplotlib with libpng 1.5, cannot be installed through normal means, and using a keg-only install of libpng 1.2 solves the issue nicely

---
+++ head-only/matplotlib.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Matplotlib < Formula
+  homepage 'http://matplotlib.sf.net'
+  head 'git://github.com/matplotlib/matplotlib.git'
+
+  depends_on 'python'
+  depends_on 'numpy' => :python
+  depends_on 'libpng12'
+  
+
+  def install
+    system "python setup.py build"
+    system "python setup.py install"
+  end
+
+  def test
+    system "python -c 'import matplotlib'"
+  end
+
+  def caveats; <<-EOS.undent
+    To uninstall matplotlib, use 'pip uninstall matplotlib'
+    EOS
+  end
+end
